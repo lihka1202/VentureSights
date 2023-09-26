2ac5da7ed3ec97640818056b31c476225e6a9658
lihka1202 <88475310+lihka1202@users.noreply.github.com>
2023-09-26 19:19:36 +0300

    refactor!: Code has been OOPd for easer integration.

--- llm.py
+++ llm.py
@@ -5,34 +5,58 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import ElasticVectorSearch, Pinecone, Weaviate, FAISS
 from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
-import os
-load_dotenv()
-print(os.environ.get("OPENAI_API_KEY"))
+import time
 
-input_data = PdfReader("testing_deck.pdf")
-rawUnformattedText = ''
-for i,page in enumerate(input_data.pages):
-    text = page.extract_text()
-    if text:
-        rawUnformattedText += text
+class OpenAIAPI:
+    def __init__(self, queries) -> None:
+        load_dotenv()
+        self.queries = queries
+    
+    def read_data(self) -> str:
+        input_data = PdfReader("testing_deck.pdf")
+        rawUnformattedText = ''
+        for i,page in enumerate(input_data.pages):
+            text = page.extract_text()
+            if text:
+                rawUnformattedText += text
+        return rawUnformattedText
+    
+    def splitIntoChunks(self, rawUnformattedText : str):
+        splittingMechanism = CharacterTextSplitter(
+            separator="\n",
+            chunk_size = 1000,
+            chunk_overlap = 200,
+            length_function = len,
+        )
 
-splittingMechanism = CharacterTextSplitter(
-    separator="\n",
-    chunk_size = 1000,
-    chunk_overlap = 200,
-    length_function = len,
-)
+        return splittingMechanism.split_text(rawUnformattedText)
+    
+    def generateResponses(self, formattedTexts):
+        embeddings = OpenAIEmbeddings()
+        docsearch = FAISS.from_texts(texts=formattedTexts, embedding=embeddings)
+        chain = load_qa_chain(OpenAI(), chain_type="stuff")
+        for query in self.queries:
+            print(f"The query is: {query}")
+            docs = docsearch.similarity_search(query)
+            print(f"The answer is: {chain.run(input_documents=docs, question=query)}")
+            print("\n")
+            time.sleep(10)
 
-formattedTexts = splittingMechanism.split_text(rawUnformattedText)
-print(len(formattedTexts))
+    def run(self):
+        rawData = self.read_data()
+        formattedData = self.splitIntoChunks(rawUnformattedText=rawData)
+        self.generateResponses(formattedTexts=formattedData)
 
-embeddings = OpenAIEmbeddings()
+# query = "who are the team members of this startup?"
+# docs = docsearch.similarity_search(query)
+# output = chain.run(input_documents=docs, question=query)
+# print(output)
 
-docsearch = FAISS.from_texts(texts=formattedTexts, embedding=embeddings)
-
-chain = load_qa_chain(OpenAI(), chain_type="stuff")
-
-query = "who are the team members of this startup?"
-docs = docsearch.similarity_search(query)
-output = chain.run(input_documents=docs, question=query)
-print(output)
+if __name__=="__main__":
+    queries = [
+        "who are the team members of this startup?",
+        "What is the Go To Market Strategy",
+        "Explain the Value proposition"
+    ]
+    testerObj = OpenAIAPI(queries=queries)
+    testerObj.run()
