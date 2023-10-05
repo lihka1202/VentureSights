8f13de14ea6dfab9bf691f70f7b90f1a9e6d0400
ryanquek22 <112756792+ryanquek22@users.noreply.github.com>
2023-10-06 05:51:28 +0800

    feat! Landing page Initial commit

---
+++ landingPage/reactfirst/src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
+function App() {
+  return (
+    <body class="text-center">
+  <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+  <header class="masthead mb-auto">
+    <div class="inner">
+      <h3 class="masthead-brand">Venturesights</h3>
+        <nav class="nav nav-masthead justify-content-center">
+          <a class="nav-link active" href="https://www.linkedin.com/in/akhil-muthya-narayanachary/">Akhil Muthya Narayanachary</a>
+          <a class="nav-link" href="https://www.linkedin.com/in/ryan-quek-596a1620a">Ryan Quek Wei Heng</a>
+          <a class="nav-link" href="https://www.linkedin.com/in/gohzhengteck/">Goh Zheng Teck</a>
+        </nav>
+      </div>
+    </header>
+
+    <main role="main" class="inner cover">
+      <h1 class="cover-heading">Our vision - Due Dilligence at your fingertips.</h1>
+      <p class="lead">Research Objective</p>
+      <p class="lead">Contact us - venturesights@gmail.com</p>
+      <p class="lead">Partners Reichman University Israel</p>
+    </main>
+  </div>
+</body>
+
+  );
+}
+
+export default App;
